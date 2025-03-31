c40c78425c17796a2a2cdeb3f9a114c63f3bbfca
Juca004 <160726460+Juliana004@users.noreply.github.com>
2025-03-31 15:22:19 +0100

    tudo

--- SD/obj/Debug/net8.0/SD.AssemblyInfo.cs
+++ SD/obj/Debug/net8.0/SD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+953d4dc4756e8c990cd6b98bd19a34918f16922a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a60a4c0a2e42b0d3379ac0ba65b7017f7492e334")]
 [assembly: System.Reflection.AssemblyProductAttribute("SD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
