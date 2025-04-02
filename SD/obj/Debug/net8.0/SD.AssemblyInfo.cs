dbc2f3f2edcb9d76d2f514337890ed5643e03b59
mtorrescoelho <120043088+torrescoelho@users.noreply.github.com>
2025-04-02 14:49:31 +0100

    entrei

--- SD/obj/Debug/net8.0/SD.AssemblyInfo.cs
+++ SD/obj/Debug/net8.0/SD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a60a4c0a2e42b0d3379ac0ba65b7017f7492e334")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7716b433779e0cbe77d2a842f783834d70d50bd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
