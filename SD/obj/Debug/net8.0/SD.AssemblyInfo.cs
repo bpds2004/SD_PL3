5eb9e8979d27941c5753ff78ce8515b642929fd3
mtorrescoelho <120043088+torrescoelho@users.noreply.github.com>
2025-04-11 11:41:32 +0100

    tentativa guardar dados falhada

--- SD/obj/Debug/net8.0/SD.AssemblyInfo.cs
+++ SD/obj/Debug/net8.0/SD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e0dc8907956375317386df13588621a721a0962")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc9b247ef17e206f562a8be8dad6bb8eecee5e48")]
 [assembly: System.Reflection.AssemblyProductAttribute("SD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
