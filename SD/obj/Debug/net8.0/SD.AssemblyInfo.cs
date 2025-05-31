6a430d2bf5d10a9e19ea27b5cd438d8661b62923
mtorrescoelho <120043088+torrescoelho@users.noreply.github.com>
2025-05-31 19:42:41 +0100

    confirmar tudo

--- SD/obj/Debug/net8.0/SD.AssemblyInfo.cs
+++ SD/obj/Debug/net8.0/SD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a60a4c0a2e42b0d3379ac0ba65b7017f7492e334")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3888f8d1333938a52ec103081c704bf297d016c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
